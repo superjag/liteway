86c6b7c58b0ddc1f268a704a1e6cdb90c02689de
Jonathan Graef <superjag@sbcglobal.net>
2014-09-06 07:34:44 -0500

    more downloader/updater work

---
+++ liteway/programs/update.lua
@@ -0,0 +1,36 @@
+--name update
+
+local args = {...}
+
+if #args==0 then
+ print("Must specify file to update")
+ return
+end
+
+local filename = shell.resolve(table.concat(args, " "))
+
+if not fs.exists(filename) or fs.isDir(filename) then
+ print("No file")
+ return
+end
+
+local updateURL = nil
+local oldExternalFiles = {}
+local updateCommands = {}
+
+local file = fs.open(filename, "r")
+local line = file.readLine()
+while line~=nil do
+ if line:sub(1, 13)=="--update-url " and line:len()>13 then
+  if updateURL then
+   print("Error: Multiple --update-urls")
+   return
+  end
+  updateURL = line:sub(14, line:len())
+ elseif line:sub(1, 17)=="--update-command " then
+  
+ elseif line:sub(1, 16)=="--external-file " then
+  
+ end
+ line = file.readLine()
+end
